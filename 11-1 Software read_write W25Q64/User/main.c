b17ce7aef00b3083b1c51a90cb462bc1df95841e
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2024-12-09 21:43:46 +0800

    practice

---
+++ 11-1 Software read_write W25Q64/User/main.c
@@ -0,0 +1,11 @@
+#include "stm32f10x.h"
+#include "Delay.h"
+#include "OLED.h"
+#include "Key.h"
+
+int main(void)
+{
+	while (1)
+	{
+	}
+} // 所以文件中最后一行必须是空行，否则会报警告: last line of file ends without a newline
